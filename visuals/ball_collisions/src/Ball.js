37ee59ebf2d5d1f0be921e97527e80c75696a78d
LonelyCpp <ananthu.kanive6@gmail.com>
2019-08-01 13:03:00 +0530

    add basic drawing

--- visuals/ball_collisions/src/Ball.js
+++ visuals/ball_collisions/src/Ball.js
@@ -1,11 +1,16 @@
 class Ball {
   constructor(x, y, radius) {
+    this.radius = radius;
     this.body = Matter.Bodies.circle(x, y, radius, {
       friction: 0.2,
       restitution: 0.8,
       frictionAir: 0
     });
   }
+
+  draw = () => {
+    circle(this.body.position.x, this.body.position.y, this.radius * 2);
+  };
 
   pushLeft = magnitude => {
     const force = Matter.Vector.create(-magnitude * 0.01, 0);
--- visuals/ball_collisions/src/Wall.js
+++ visuals/ball_collisions/src/Wall.js
@@ -1,9 +1,20 @@
 class Wall {
   constructor(x, y, width, height) {
+    this.width = width;
+    this.height = height;
     this.body = Matter.Bodies.rectangle(x, y, width, height, {
       isStatic: true,
       friction: 0,
       frictionStatic: 0
     });
+    console.log(this.body);
   }
+  draw = () => {
+    rect(
+      this.body.vertices[0].x,
+      this.body.vertices[0].y,
+      this.width,
+      this.height
+    );
+  };
 }
